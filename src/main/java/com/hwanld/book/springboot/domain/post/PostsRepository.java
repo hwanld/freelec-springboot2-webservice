95270db0b974e4512d42f6cce444791ff311c5b9
wkazxf <wkazxf@gmail.com>
2022-01-10 21:04:09 +0900

    with .gitignore clear the cache and add all the files

--- src/main/java/com/hwanld/book/springboot/domain/post/PostsRepository.java
+++ src/main/java/com/hwanld/book/springboot/domain/post/PostsRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
+    //With interface Spring JPA
     @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
     List<Posts> findAllDesc();
 }
