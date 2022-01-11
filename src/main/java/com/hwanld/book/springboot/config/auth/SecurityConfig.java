ab2bef75483fa2a34653fa64dc0aaf8eaf08bbf0
wkazxf <wkazxf@gmail.com>
2022-01-12 00:10:05 +0900

    Setting Spring Security

---
+++ src/main/java/com/hwanld/book/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.hwanld.book.springboot.config.auth;
+
+import com.hwanld.book.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService extends WebSecurityConfigurerAdapter {
+        private final CustomOAuth2UserService customOAuth2UserService;
+        @Override
+                protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .csrf().disable()
+                    .headers().frameOptions().disable()
+                    .and()
+                        .authorizeRequests()
+                        .andMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                        .andMatchers("/api/v1/**").hasRole(Role.USER.name())
+                        .anyRequest().authenticated()
+                    .and()
+                        .logout()
+                            .logoutSuccessUrl("/")
+                    .and()
+                        .oauth2Login()
+                            .userInfoEndpoint()
+                                .userService(customOAuth2UserService);
+
+        }
+    }
+}
