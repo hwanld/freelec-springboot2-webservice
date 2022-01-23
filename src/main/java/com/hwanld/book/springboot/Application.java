d11f4d948f347e3fd39b51751c13b036e85f6557
donghwan <wkazxf@gmail.com>
2022-01-23 22:42:29 +0900

    Set Spring Security completely

--- src/main/java/com/hwanld/book/springboot/Application.java
+++ src/main/java/com/hwanld/book/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//Changed from window to mac
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class Application {
