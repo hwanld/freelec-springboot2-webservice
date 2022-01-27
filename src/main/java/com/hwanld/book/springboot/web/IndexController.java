c25de4316a962e069901cee9df35a16b76a249c2
donghwan <wkazxf@gmail.com>
2022-01-28 08:43:36 +0900

    세션 저장소로 데이터베이스 사용하기

--- src/main/java/com/hwanld/book/springboot/web/IndexController.java
+++ src/main/java/com/hwanld/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.hwanld.book.springboot.web;
 
+import com.hwanld.book.springboot.config.auth.LoginUser;
 import com.hwanld.book.springboot.config.auth.dto.SessionUser;
 import com.hwanld.book.springboot.service.posts.PostsService;
 import com.hwanld.book.springboot.web.dto.PostsResponseDto;
@@ -17,16 +18,15 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-        if (user != null) {
+        if(user != null) {
             model.addAttribute("userName", user.getName());
         }
+
         return "index";
     }
 
